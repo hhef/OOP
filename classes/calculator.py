c3546cbf9054f0ede93c88d487edaee6222e4506
heff <ksuder7@gmail.com>
2019-11-20 20:23:04 +0100

    change prints in calculator class

--- classes/calculator.py
+++ classes/calculator.py
@@ -4,27 +4,27 @@ class Calculator:
 
     def addition(self, num1, num2):
         result = num1 + num2
-        self.history.append(f"added {num1} to {num2} and got {result}")
+        self.history.append(f"Added {num1} to {num2} and got {result}")
         return result
 
     def subtract(self, num1, num2):
         result = num1 - num2
-        self.history.append(f"subtracted {num1} from {num2} and got {result}")
+        self.history.append(f"Subtracted {num1} from {num2} and got {result}")
         return result
 
     def multiply(self, num1, num2):
         result = num1 * num2
-        self.history.append(f"multiplied {num1} by {num2} and got {result}")
+        self.history.append(f"Multiplied {num1} by {num2} and got {result}")
         return result
 
     def division(self, num1, num2):
         result = num1 / num2
-        self.history.append(f"divided {num1} by {num2} and got {result}")
+        self.history.append(f"Divided {num1} by {num2} and got {result}")
         return result
 
     def print_history(self):
         for index, operation in enumerate(self.history, 1):
-            print(index, "-", operation)
+            print(f"{index}. {operation}")
 
 
 calc = Calculator()
